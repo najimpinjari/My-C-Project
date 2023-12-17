228e9c3ff1a75968d99f47a118cd06268c9e1fb6
NAJIMdp41 <143899549+najimpinjari@users.noreply.github.com>
2023-12-17 12:32:56 +0530

    Add files via upload

---
+++ Basic Chat Application/Program.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net.Sockets;
+using System.Net;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Basic_Chat_Application
+{
+    class Program
+    {
+        static async Task Main()
+        {
+            TcpListener server = new TcpListener(IPAddress.Any, 8888);
+            server.Start();
+            Console.WriteLine("Server started. Waiting for connections...");
+
+            TcpClient client = await server.AcceptTcpClientAsync();
+            Console.WriteLine("Client connected!");
+
+            NetworkStream stream = client.GetStream();
+            while (true)
+            {
+                byte[] buffer = new byte[1024];
+                int bytesRead = await stream.ReadAsync(buffer, 0, buffer.Length);
+                string message = Encoding.UTF8.GetString(buffer, 0, bytesRead);
+                Console.WriteLine($"Client: {message}");
+
+                Console.Write("Server: ");
+                string response = Console.ReadLine();
+                byte[] responseBytes = Encoding.UTF8.GetBytes(response);
+                await stream.WriteAsync(responseBytes, 0, responseBytes.Length);
+            }
+        }
+    }
+}
