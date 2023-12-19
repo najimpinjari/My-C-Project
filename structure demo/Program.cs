ad181ecd27b509a3bbf950e9fe1fcb303bdc0687
NAJIMdp41 <143899549+najimpinjari@users.noreply.github.com>
2023-12-19 18:23:07 +0530

    Add files via upload

---
+++ structure demo/Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace structure_demo
+{
+     struct Program
+    {   
+
+        public void fank1() //method onw 
+        {
+            Console.WriteLine("this is my first function "); //method 
+        }
+        static void Main(string[] args)
+        {
+            Program p = new Program(); //obj of fanc one class 
+            p.fank1();
+
+            Console.ReadLine();
+        }
+    }
+}
