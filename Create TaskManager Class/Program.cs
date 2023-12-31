94e6068e7c0d8481d7c451796b461e1ceef68207
NAJIMdp41 <143899549+najimpinjari@users.noreply.github.com>
2023-12-31 10:55:31 +0530

    Add files via upload

---
+++ Create TaskManager Class/Program.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Create_TaskManager_Class
+{
+    class TaskManager
+    {
+        private List<Task> tasks = new List<Task>();
+
+        public void AddTask(string description)
+        {
+            tasks.Add(new Task { Description = description, IsCompleted = false });
+        }
+
+        public void MarkTaskAsCompleted(int index)
+        {
+            if (index >= 0 && index < tasks.Count)
+            {
+                tasks[index].IsCompleted = true;
+                Console.WriteLine("Task marked as completed: " + tasks[index].Description);
+            }
+            else
+            {
+                Console.WriteLine("Invalid task index");
+            }
+        }
+
+        public void DisplayTasks()
+        {
+            Console.WriteLine("Task List:");
+            for (int i = 0; i < tasks.Count; i++)
+            {
+                Console.WriteLine($"{i + 1}. [{(tasks[i].IsCompleted ? "X" : " ")}] {tasks[i].Description}");
+            }
+        }
+    }
+
+}
